315e867fcea402c3cdce0d5a9cd45910d0fa2a3b
gotoweb <devnull@localhost>
2011-04-12 13:07:03 +0000

    bundle can view bundle list

--- kraken-webconsole/src/main/resources/WEB-INF/core/bundle.js
+++ kraken-webconsole/src/main/resources/WEB-INF/core/bundle.js
@@ -2,6 +2,7 @@
     this.name = "Bundle";
 
     this.onstart = function (pid, args) {
+		programManager.loadJS('/js/ext/Ext.DataView.DragSelector.js');
         var that = this;
 		
 		var StorePackage = new Ext.data.ArrayStore({
@@ -13,69 +14,79 @@
             }
         });
 		
+		var StoreBundle = new Ext.data.ArrayStore({
+			proxy: new Ext.data.MemoryProxy(),
+			fields: ['id', 'last_modified', 'status', 'location', 'vendor', 'name', 'built_by', 'license', 'export_package', 'url', 'import_package', 'version'],
+			sortInfo: {
+				field: 'name',
+				direction: 'ASC'
+			}
+		});
+		
 		function getInstalledPackages(p) {
 			var pnl = p.items.items[0];
 			var tbar = pnl.getTopToolbar();
 			channel.send(1, 'org.krakenapps.webconsole.plugins.PackagePlugin.getInstalledPackages', {},
 				function(resp) {
-					resp = {
-    "packages": [
-        {
-            "description": null,
-            "name": "kraken-siem",
-            "date": "2011-01-17 19:43:00+0900",
-            "version": "1.0.0"
-        },
-		{
-            "description": null,
-            "name": "kraken-zxcv",
-            "date": "2011-01-17 19:43:00+0900",
-            "version": "1.0.0"
-        },
-		{
-            "description": null,
-            "name": "kraken-qwer",
-            "date": "2011-01-17 19:43:00+0900",
-            "version": "1.0.0"
-        },{
-            "description": null,
-            "name": "kraken-hhh",
-            "date": "2011-01-17 19:43:00+0900",
-            "version": "1.0.0"
-        },{
-            "description": null,
-            "name": "kraken-qqq",
-            "date": "2011-01-17 19:43:00+0900",
-            "version": "1.0.0"
-        },{
-            "description": null,
-            "name": "zxcv-siem",
-            "date": "2011-01-17 19:43:00+0900",
-            "version": "1.0.0"
-        },{
-            "description": null,
-            "name": "qwer-siem",
-            "date": "2011-01-17 19:43:00+0900",
-            "version": "1.0.0"
-        },{
-            "description": null,
-            "name": "hhh-siem",
-            "date": "2011-01-17 19:43:00+0900",
-            "version": "1.0.0"
-        },{
-            "description": null,
-            "name": "zz-siem",
-            "date": "2011-01-17 19:43:00+0900",
-            "version": "1.0.0"
-        },{
-            "description": null,
-            "name": "x-siem",
-            "date": "2011-01-17 19:43:00+0900",
-            "version": "1.0.0"
-        }
-		
-    ]
-}
+					/*resp = {
+						"packages": [
+							{
+								"description": null,
+								"name": "kraken-siem",
+								"date": "2011-01-17 19:43:00+0900",
+								"version": "1.0.0"
+							},
+							{
+								"description": null,
+								"name": "kraken-zxcv",
+								"date": "2011-01-17 19:43:00+0900",
+								"version": "1.0.0"
+							},
+							{
+								"description": null,
+								"name": "kraken-qwer",
+								"date": "2011-01-17 19:43:00+0900",
+								"version": "1.0.0"
+							},{
+								"description": null,
+								"name": "kraken-hhh",
+								"date": "2011-01-17 19:43:00+0900",
+								"version": "1.0.0"
+							},{
+								"description": null,
+								"name": "kraken-qqq",
+								"date": "2011-01-17 19:43:00+0900",
+								"version": "1.0.0"
+							},{
+								"description": null,
+								"name": "zxcv-siem",
+								"date": "2011-01-17 19:43:00+0900",
+								"version": "1.0.0"
+							},{
+								"description": null,
+								"name": "qwer-siem",
+								"date": "2011-01-17 19:43:00+0900",
+								"version": "1.0.0"
+							},{
+								"description": null,
+								"name": "hhh-siem",
+								"date": "2011-01-17 19:43:00+0900",
+								"version": "1.0.0"
+							},{
+								"description": null,
+								"name": "zz-siem",
+								"date": "2011-01-17 19:43:00+0900",
+								"version": "1.0.0"
+							},{
+								"description": null,
+								"name": "x-siem",
+								"date": "2011-01-17 19:43:00+0900",
+								"version": "1.0.0"
+							}
+							
+						]
+					}*/
+					
 					// storing
 					var arr = [];
 					Ext.each(resp.packages, function (obj, i) {
@@ -133,7 +144,86 @@
 				}
 			);
 		}
+
+		function getBundles(p) {
+			var pnl = p.items.items[0];
+			channel.send(1, 'org.krakenapps.webconsole.plugins.BundlePlugin.getBundles', {},
+				function(resp) {
+					
+					// storing
+					var arr = [];
+					Ext.each(resp.bundles, function (obj, i) {
+						var p = [];
+						for (prop in obj)
+							p.push(obj[prop]);
+							
+						arr.push(p);
+					});
+					StoreBundle.loadData(arr);
+					
+					// drawing dataview
+					pnl.add({
+						xtype: 'dataview',
+						store: StoreBundle,
+						tpl: new Ext.XTemplate('<ul>', '<tpl for=".">', '<li class="bdleicon">', '<img width="48" height="48" src="img/bundle_64.png" />',
+							'<strong>{name}</strong>', '<span>{version}</span>' ,
+							'<div class="rightbutton"><span style="border: 1px solid #aaa; padding: 3px; -webkit-border-radius: 3px; background-image: -webkit-gradient(linear,left bottom,left top,color-stop(0.02, rgb(224,224,224)),color-stop(0.52, rgb(255,255,255)));">Stop</span></div>',
+							'</li>', '</tpl>', '</ul>'),
+						itemSelector: 'li.bdleicon',
+						overClass: 'bdleicon-hover',
+						selectedClass: 'bdleicon-selected',
+						singleSelect: true,
+						multiSelect: true,
+						autoScroll: true,
+						listeners: {
+							selectionchange: function(d, sel) {
+								console.log(sel);
+							}
+						},
+						plugins: [
+							new Ext.DataView.DragSelector()
+						]
+					});
+					
+					pnl.doLayout();
+				}
+			);
+		}
 		
+		function makeBundleToolbar(p) {
+			var pnl = p.items.items[0];
+			var tbar = pnl.getTopToolbar();
+			
+			// making toolbar
+			var arr = [];
+			arr.push('<span style="padding-left: 5px">Filter by Name:</span>');
+			var txtFilterName = new Ext.form.TextField({
+				labelWidth: 0,
+				emptyText: 'Type Program Name',
+				fieldCls: 'AppsSearchTextfield',
+				enableKeyEvents: true,
+				listeners: {
+					keyup: function(c, e) {
+						var value = c.getValue().toLowerCase();
+						var store = StoreBundle;
+						store.suspendEvents();
+						store.clearFilter();
+						store.resumeEvents();
+						
+						console.log('---------');
+						store.filterBy(function(rec) {
+							return rec.get('name').toLowerCase().search(value) != -1;
+						});
+						
+						store.sort('name', 'ASC');
+					}
+				}
+			});
+			arr.push(txtFilterName);
+			
+			tbar.removeAll();
+			tbar.add(arr);
+		}
 		
         var MainUI = new Ext.TabPanel({
             border: false,
@@ -168,7 +258,19 @@
                 })]
             },
 			{
-                title: 'Bundles'
+                title: 'Bundles',
+				layout: 'fit',
+				listeners: {
+					activate: getBundles,
+					afterrender: makeBundleToolbar
+				},
+                items: [
+                new Ext.Panel({
+                    layout: 'fit',
+                    items: [],
+                    bodyCls: 'AppsBox',
+                    tbar: []
+                })]
             }]
         });
 
---
+++ kraken-webconsole/src/main/resources/WEB-INF/js/ext/Ext.DataView.DragSelector.js
@@ -0,0 +1,96 @@
+/*!
+ * Ext JS Library 3.3.1
+ * Copyright(c) 2006-2010 Sencha Inc.
+ * licensing@sencha.com
+ * http://www.sencha.com/license
+ */
+
+Ext.DataView.DragSelector = function(cfg){
+    cfg = cfg || {};
+    var view, proxy, tracker;
+    var rs, bodyRegion, dragRegion = new Ext.lib.Region(0,0,0,0);
+    var dragSafe = cfg.dragSafe === true;
+
+    this.init = function(dataView){
+        view = dataView;
+        view.on('render', onRender);
+    };
+
+    function fillRegions(){
+        rs = [];
+        view.all.each(function(el){
+            rs[rs.length] = el.getRegion();
+        });
+        bodyRegion = view.el.getRegion();
+    }
+
+    function cancelClick(){
+        return false;
+    }
+
+    function onBeforeStart(e){
+        return !dragSafe || e.target == view.el.dom;
+    }
+
+    function onStart(e){
+        view.on('containerclick', cancelClick, view, {single:true});
+        if(!proxy){
+            proxy = view.el.createChild({cls:'x-view-selector'});
+        }else{
+            if(proxy.dom.parentNode !== view.el.dom){
+                view.el.dom.appendChild(proxy.dom);
+            }
+            proxy.setDisplayed('block');
+        }
+        fillRegions();
+        view.clearSelections();
+    }
+
+    function onDrag(e){
+        var startXY = tracker.startXY;
+        var xy = tracker.getXY();
+
+        var x = Math.min(startXY[0], xy[0]);
+        var y = Math.min(startXY[1], xy[1]);
+        var w = Math.abs(startXY[0] - xy[0]);
+        var h = Math.abs(startXY[1] - xy[1]);
+
+        dragRegion.left = x;
+        dragRegion.top = y;
+        dragRegion.right = x+w;
+        dragRegion.bottom = y+h;
+
+        dragRegion.constrainTo(bodyRegion);
+        proxy.setRegion(dragRegion);
+
+        for(var i = 0, len = rs.length; i < len; i++){
+            var r = rs[i], sel = dragRegion.intersect(r);
+            if(sel && !r.selected){
+                r.selected = true;
+                view.select(i, true);
+            }else if(!sel && r.selected){
+                r.selected = false;
+                view.deselect(i);
+            }
+        }
+    }
+
+    function onEnd(e){
+        if (!Ext.isIE) {
+            view.un('containerclick', cancelClick, view);    
+        }        
+        if(proxy){
+            proxy.setDisplayed(false);
+        }
+    }
+
+    function onRender(view){
+        tracker = new Ext.dd.DragTracker({
+            onBeforeStart: onBeforeStart,
+            onStart: onStart,
+            onDrag: onDrag,
+            onEnd: onEnd
+        });
+        tracker.initEl(view.el);
+    }
+};
