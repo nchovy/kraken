55d9517692213f6e2a43f0cc957f73d1b0d09e74
stania <devnull@localhost>
2011-04-12 10:25:43 +0000

    moved surplus item to trunk.

---
+++ new.bat
@@ -0,0 +1,1 @@
+mvn archetype:generate -DarchetypeGroupId=org.krakenapps -DarchetypeArtifactId=kraken-template -DarchetypeVersion=1.0.1 -DgroupId=org.krakenapps -DartifactId=%1 -Dversion=1.0.0 -Dpackage=%2
