73cd24cd4c708859f96a01dfdee9d8106bb5c9f6
gotoweb <devnull@localhost>
2011-04-14 10:17:10 +0000

    added bundle grid view

--- kraken-webconsole/src/main/resources/WEB-INF/core/bundle.js
+++ kraken-webconsole/src/main/resources/WEB-INF/core/bundle.js
@@ -111,42 +111,48 @@
 					});
 					
 					pnl.doLayout();
-					
-					// making toolbar
-					var arr = [];
-					arr.push('<span style="padding-left: 5px">Filter by Name:</span>');
-					var txtFilterName = new Ext.form.TextField({
-						labelWidth: 0,
-						emptyText: 'Type Program Name',
-						fieldCls: 'AppsSearchTextfield',
-						enableKeyEvents: true,
-						listeners: {
-							keyup: function(c, e) {
-								var value = c.getValue().toLowerCase();
-								var store = StorePackage;
-								store.suspendEvents();
-								store.clearFilter();
-								store.resumeEvents();
-								
-								console.log('---------');
-								store.filterBy(function(rec) {
-									return rec.get('name').toLowerCase().search(value) != -1;
-								});
-								
-								store.sort('name', 'ASC');
-							}
-						}
-					});
-					arr.push(txtFilterName);
-					
-					tbar.removeAll();
-					tbar.add(arr);
 				}
 			);
+		}
+		
+		function makePackageToolbar(p) {
+			var pnl = p.items.items[0];
+			var tbar = pnl.getTopToolbar();
+			
+			// making toolbar
+			var arr = [];
+			arr.push('<span style="padding-left: 5px">Filter by Name:</span>');
+			var txtFilterName = new Ext.form.TextField({
+				labelWidth: 0,
+				emptyText: 'Type Program Name',
+				fieldCls: 'AppsSearchTextfield',
+				enableKeyEvents: true,
+				listeners: {
+					keyup: function(c, e) {
+						var value = c.getValue().toLowerCase();
+						var store = StorePackage;
+						store.suspendEvents();
+						store.clearFilter();
+						store.resumeEvents();
+						
+						console.log('---------');
+						store.filterBy(function(rec) {
+							return rec.get('name').toLowerCase().search(value) != -1;
+						});
+						
+						store.sort('name', 'ASC');
+					}
+				}
+			});
+			arr.push(txtFilterName);
+			
+			tbar.removeAll();
+			tbar.add(arr);
 		}
 
 		function getBundles(p) {
 			var pnl = p.items.items[0];
+			
 			channel.send(1, 'org.krakenapps.webconsole.plugins.BundlePlugin.getBundles', {},
 				function(resp) {
 					
@@ -154,7 +160,7 @@
 					var arr = [];
 					Ext.each(resp.bundles, function (obj, i) {
 						var p = [];
-						for (prop in obj)
+						for (prop in obj) 
 							p.push(obj[prop]);
 							
 						arr.push(p);
@@ -186,13 +192,16 @@
 					});
 					
 					pnl.doLayout();
+					
+					// gridpanel
+					//StoreBundle.loadData(resp);
+					//BundleGridView.
 				}
 			);
 		}
 		
 		function makeBundleToolbar(p) {
-			var pnl = p.items.items[0];
-			var tbar = pnl.getTopToolbar();
+			var tbar = p.getTopToolbar();
 			
 			// making toolbar
 			var arr = [];
@@ -201,6 +210,7 @@
 				labelWidth: 0,
 				emptyText: 'Type Program Name',
 				fieldCls: 'AppsSearchTextfield',
+				style: 'margin-right: 5px',
 				enableKeyEvents: true,
 				listeners: {
 					keyup: function(c, e) {
@@ -221,9 +231,44 @@
 			});
 			arr.push(txtFilterName);
 			
+			arr.push('-');
+			arr.push('View: ');
+			arr.push({
+				xtype: 'button',
+				text: 'Icon',
+				enableToggle: true,
+				toggleGroup: 'viewtype',
+				handler: function() {
+					that.BundleTab.layout.setActiveItem(0);
+				}
+			});
+			
+			arr.push({
+				xtype: 'button',
+				text: 'Grid',
+				enableToggle: true,
+				toggleGroup: 'viewtype',
+				pressed: true,
+				handler: function() {
+					console.log(that.BundleTab);
+					that.BundleTab.layout.setActiveItem(1);
+				}
+			});
+			
 			tbar.removeAll();
 			tbar.add(arr);
 		}
+		
+		function renderStatus(val) {
+			if (val == 'ACTIVE') {
+				return '<span style="color:green;">' + val + '</span>';
+			} else {
+				return '<span style="color:red;">' + val + '</span>';
+			}
+			return val;
+		}
+		
+		
 		
         var MainUI = new Ext.TabPanel({
             border: false,
@@ -247,7 +292,8 @@
                 title: 'Packages',
                 layout: 'fit',
 				listeners: {
-					activate: getInstalledPackages
+					activate: getInstalledPackages,
+					afterrender: makePackageToolbar
 				},
                 items: [
                 new Ext.Panel({
@@ -257,21 +303,60 @@
                     tbar: []
                 })]
             },
-			{
+			that.BundleTab = new Ext.Panel({
                 title: 'Bundles',
-				layout: 'fit',
+				layout: 'card',
+				tbar: [],
 				listeners: {
 					activate: getBundles,
 					afterrender: makeBundleToolbar
 				},
                 items: [
-                new Ext.Panel({
-                    layout: 'fit',
-                    items: [],
-                    bodyCls: 'AppsBox',
-                    tbar: []
-                })]
-            }]
+					that.BundleIconView = new Ext.Panel({
+						layout: 'fit',
+						items: [],
+						bodyCls: 'AppsBox'
+					}),
+					that.BundleGridView = new Ext.grid.GridPanel({
+						store: StoreBundle,
+						viewConfig: { forceFit: true },
+						columns: [
+							{
+								xtype: 'gridcolumn',
+								dataIndex: 'id',
+								header: 'ID',
+								sortable: true,
+								resizable: true,
+								width: 50,
+								align: 'right'
+							},
+							{
+								xtype: 'gridcolumn',
+								dataIndex: 'name',
+								header: 'Symbolic Name',
+								sortable: true,
+								width: 250
+							},
+							{
+								xtype: 'gridcolumn',
+								header: 'Version',
+								sortable: true,
+								width: 100,
+								dataIndex: 'version'
+							},
+							{
+								xtype: 'gridcolumn',
+								header: 'Status',
+								renderer: renderStatus,
+								sortable: true,
+								width: 100,
+								dataIndex: 'status'
+							}
+						]
+					})
+				],
+				activeItem: 1
+            })]
         });
 
         var window = windowManager.createWindow(pid, this.name, 720, 350, MainUI);
