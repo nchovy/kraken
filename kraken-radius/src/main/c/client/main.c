a4f1e37505086e3b8e7296cffd7a0e42ff46ec23
xeraph <devnull@localhost>
2011-05-23 10:54:45 +0000

    removed unused code.

--- kraken-radius/src/main/c/client/main.c
+++ kraken-radius/src/main/c/client/main.c
@@ -20,14 +20,10 @@
 
 int main(int argc, char *argv[]) 
 {
-	int i = 0;
 	int ret = 0;
 	radius_client_t *client = NULL;
-	byte_t *encoded = NULL;
-	int encoded_len = 0;
 	radius_packet_t *response = NULL;
-	byte_t authenticator[] = { 0x34, 0x65, 0xa0, 0x8e, 0x9e, 0x7a, 0x9c, 0xb1, 0x50, 0x42, 0x25, 0x28, 0xb0, 0x83, 0x40, 0x86 };
-	
+
 #ifdef WIN32
 	WSADATA wsa;
 	ret = WSAStartup(MAKEWORD(2, 2), &wsa);
