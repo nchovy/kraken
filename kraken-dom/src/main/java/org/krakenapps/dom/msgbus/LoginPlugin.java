582588b794227cc2c2d83943dc2ea9d18ce808b6
xeraph <devnull@localhost>
2011-09-04 13:03:56 +0000

    return idle_timeout only if user want to use idle timeout

--- kraken-dom/src/main/java/org/krakenapps/dom/msgbus/LoginPlugin.java
+++ kraken-dom/src/main/java/org/krakenapps/dom/msgbus/LoginPlugin.java
@@ -64,7 +64,8 @@ public class LoginPlugin {
 
 		Admin admin = adminApi.login(nick, hash, nonce);
 		resp.put("result", "success");
-		resp.put("idle_timeout", admin.getIdleTimeout());
+		if (admin.isUseIdleTimeout())
+			resp.put("idle_timeout", admin.getIdleTimeout());
 
 		session.unsetProperty("nonce");
 		session.setProperty("org_id", admin.getUser().getOrganization().getId());
