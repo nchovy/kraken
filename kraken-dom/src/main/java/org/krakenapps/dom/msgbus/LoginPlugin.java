1993d3d96d6a021e5ed06b476f1d636b5827e76b
xeraph <devnull@localhost>
2011-09-10 12:38:11 +0000

    added session_id to LoginPlugin.hello() response.

--- kraken-dom/src/main/java/org/krakenapps/dom/msgbus/LoginPlugin.java
+++ kraken-dom/src/main/java/org/krakenapps/dom/msgbus/LoginPlugin.java
@@ -94,6 +94,7 @@ public class LoginPlugin {
 		req.getSession().setProperty("lang", lang);
 
 		resp.put("nonce", nonce);
+		resp.put("session_id", req.getSession().getId());
 		resp.put("message", "login please.");
 
 		EntityManager em = entityManagerService.getEntityManager();
