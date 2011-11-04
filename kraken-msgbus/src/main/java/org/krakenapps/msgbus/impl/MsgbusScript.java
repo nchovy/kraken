330f87dd2544f889d162022562893e39b88259bb
xeraph <devnull@localhost>
2011-11-05 01:33:11 +0900

    added msgbus run script command for immediate run test.

--- kraken-msgbus/src/main/java/org/krakenapps/msgbus/impl/MsgbusScript.java
+++ kraken-msgbus/src/main/java/org/krakenapps/msgbus/impl/MsgbusScript.java
@@ -1,9 +1,19 @@
 package org.krakenapps.msgbus.impl;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.krakenapps.api.Primitive;
 import org.krakenapps.api.Script;
+import org.krakenapps.api.ScriptArgument;
 import org.krakenapps.api.ScriptContext;
+import org.krakenapps.api.ScriptUsage;
+import org.krakenapps.msgbus.AbstractSession;
+import org.krakenapps.msgbus.Message;
 import org.krakenapps.msgbus.MessageBus;
 import org.krakenapps.msgbus.Session;
+import org.krakenapps.msgbus.Message.Type;
 
 public class MsgbusScript implements Script {
 	private MessageBus msgbus;
@@ -57,4 +67,74 @@ public class MsgbusScript implements Script {
 		}
 	}
 
+	@ScriptUsage(description = "run msgbus method", arguments = {
+			@ScriptArgument(name = "org id", type = "int", description = "org id"),
+			@ScriptArgument(name = "admin id", type = "int", description = "admin id"),
+			@ScriptArgument(name = "method", type = "string", description = "package.method format"),
+			@ScriptArgument(name = "arguments", type = "string", description = "key=value format arguments", optional = true) })
+	public void run(String[] args) throws InterruptedException {
+		Integer orgId = Integer.valueOf(args[0]);
+		Integer adminId = Integer.valueOf(args[1]);
+		String method = args[2];
+
+		Map<String, Object> m = new HashMap<String, Object>();
+		for (int i = 3; i < args.length; i++) {
+			String arg = args[i];
+			int pos = arg.indexOf('=');
+			String s = arg;
+			String value = null;
+			if (pos >= 0) {
+				s = arg.substring(0, pos);
+				value = arg.substring(pos + 1);
+			}
+
+			m.put(s, value);
+		}
+
+		Message msg = new Message();
+		msg.setType(Type.Request);
+		msg.setMethod(method);
+		msg.setParameters(m);
+
+		Date begin = new Date();
+		ScriptSession s = new ScriptSession(orgId, adminId);
+		msgbus.dispatch(s, msg);
+		while (true) {
+			if (s.completed)
+				break;
+
+			Date now = new Date();
+			if (now.getTime() - begin.getTime() > 5000)
+				break;
+
+			Thread.sleep(100);
+		}
+	}
+
+	private class ScriptSession extends AbstractSession {
+		private boolean completed;
+		private int orgId;
+		private int adminId;
+
+		public ScriptSession(int orgId, int adminId) {
+			this.orgId = orgId;
+			this.adminId = adminId;
+		}
+
+		@Override
+		public Integer getOrgId() {
+			return orgId;
+		}
+
+		@Override
+		public Integer getAdminId() {
+			return adminId;
+		}
+
+		@Override
+		public void send(Message msg) {
+			context.println(Primitive.stringify(msg.getParameters()));
+			completed = true;
+		}
+	}
 }
