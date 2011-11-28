a4c5e6abfb91dd639d7f9fe9cf625734ba34e2a3
xeraph <devnull@localhost>
2011-11-28 20:16:51 +0900

    fixed date column name NPE

--- kraken-logdb-pcap/src/main/java/org/krakenapps/logdb/pcap/impl/PcapCommand.java
+++ kraken-logdb-pcap/src/main/java/org/krakenapps/logdb/pcap/impl/PcapCommand.java
@@ -2,25 +2,30 @@ package org.krakenapps.logdb.pcap.impl;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.krakenapps.logdb.LogQueryCommand;
 import org.krakenapps.pcap.decoder.ethernet.EthernetFrame;
 import org.krakenapps.pcap.decoder.ethernet.EthernetProcessor;
+import org.krakenapps.pcap.packet.PacketHeader;
 import org.krakenapps.pcap.util.PcapFileRunner;
 
 public class PcapCommand extends LogQueryCommand {
 	private PcapFileRunner runner;
 
 	public PcapCommand(File f) {
+		dateColumnName = "_time";
 		runner = new PcapFileRunner(f);
 		runner.getEthernetDecoder().register(new EthernetProcessor() {
 
 			@Override
 			public void process(EthernetFrame frame) {
-				System.out.println("## eth " + frame);
 				Map<String, Object> m = new HashMap<String, Object>();
+				PacketHeader h = frame.getPcapPacket().getPacketHeader();
+				Date date = new Date(h.getTsSec() * 1000L + h.getTsUsec());
+				m.put("_time", date);
 				m.put("proto", "eth");
 				m.put("ether_type", frame.getType());
 				m.put("frame_size", frame.getData().readableBytes());
