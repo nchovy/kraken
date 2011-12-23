e025900ef5c6bb495139154afbf3bacfd8616927
delmitz <delmitz@delmitz-PC.office.nchovy.net>
2011-12-23 11:32:32 +0900

    added config database adds, updates, removes.

--- kraken-dom/src/main/java/org/krakenapps/dom/api/ConfigManager.java
+++ kraken-dom/src/main/java/org/krakenapps/dom/api/ConfigManager.java
@@ -1,6 +1,7 @@
 package org.krakenapps.dom.api;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.krakenapps.api.PrimitiveParseCallback;
 import org.krakenapps.confdb.ConfigDatabase;
@@ -12,18 +13,26 @@ public interface ConfigManager {
 	ConfigDatabase getDatabase(String domain);
 
 	<T> Collection<T> all(String domain, Class<T> cls);
-	
+
 	<T> Collection<T> all(String domain, Class<T> cls, Predicate pred);
 
 	<T> T find(String domain, Class<T> cls, Predicate pred);
 
 	<T> T get(String domain, Class<T> cls, Predicate pred, String notFoundMessage);
 
+	<T> void adds(String domain, Class<T> cls, List<Predicate> preds, List<T> docs, String alreadyExistMessage,
+			DefaultEntityEventProvider<T> provider);
+
 	<T> void add(String domain, Class<T> cls, Predicate pred, T doc, String alreadyExistMessage, DefaultEntityEventProvider<T> provider);
+
+	<T> void updates(String domain, Class<T> cls, List<Predicate> preds, List<T> docs, String notFoundMessage,
+			DefaultEntityEventProvider<T> provider);
 
 	<T> void update(String domain, Class<T> cls, Predicate pred, T doc, String notFoundMessage, DefaultEntityEventProvider<T> provider);
 
+	<T> void removes(String domain, Class<T> cls, List<Predicate> preds, String notFoundMessage, DefaultEntityEventProvider<T> provider);
+
 	<T> void remove(String domain, Class<T> cls, Predicate pred, String notFoundMessage, DefaultEntityEventProvider<T> provider);
-	
+
 	PrimitiveParseCallback getParseCallback(String domain);
 }
