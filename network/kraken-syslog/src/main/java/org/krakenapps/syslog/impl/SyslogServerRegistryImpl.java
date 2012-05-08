46f690d23cc52ea3e12e35369ffc1d275b436990
cloudz <devnull@localhost>
2012-05-08 12:49:49 +0900

    * method: close()         add server.removeListener(this)

--- network/kraken-syslog/src/main/java/org/krakenapps/syslog/impl/SyslogServerRegistryImpl.java
+++ network/kraken-syslog/src/main/java/org/krakenapps/syslog/impl/SyslogServerRegistryImpl.java
@@ -236,8 +236,11 @@ public class SyslogServerRegistryImpl implements SyslogServerRegistry, SyslogLis
 	@Override
 	public void close(String name) {
 		SyslogServer server = serverMap.remove(name);
-		if (server != null)
+		if (server != null) {
+			// remove callback
+			server.removeListener(this);
 			server.close();
+		}
 
 		ConfigDatabase db = conf.ensureDatabase("kraken-syslog");
 		Config c = db.findOne(SyslogProfile.class, Predicates.field("name", name));
