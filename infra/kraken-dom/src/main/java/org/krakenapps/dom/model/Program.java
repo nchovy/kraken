022dba22040c4fbe2ff869516b1a021f879afd25
darkluster <darkluster@DARKLUSTER.office.nchovy.net>
2012-05-30 15:50:38 +0900

    * added Program.equals() and Program.hashcode()  * added dom.resetPassword command

--- infra/kraken-dom/src/main/java/org/krakenapps/dom/model/Program.java
+++ infra/kraken-dom/src/main/java/org/krakenapps/dom/model/Program.java
@@ -105,6 +105,43 @@ public class Program {
 
 	public void setUpdated(Date updated) {
 		this.updated = updated;
+	}	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((pack == null) ? 0 : pack.hashCode());
+		result = prime * result + ((path == null) ? 0 : path.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Program other = (Program) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (pack == null) {
+			if (other.pack != null)
+				return false;
+		} else if (!pack.equals(other.pack))
+			return false;
+		if (path == null) {
+			if (other.path != null)
+				return false;
+		} else if (!path.equals(other.path))
+			return false;
+		return true;
 	}
 
 	@Override
