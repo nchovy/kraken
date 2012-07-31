c93bf82012d0dec30bd6fb9293c9fc07f64a4e4e
wghwang <devnull@localhost>
2012-07-31 16:48:31 +0900

    build fix.

--- reporting/kraken-docxcod/src/main/java/org/krakenapps/docxcod/FreeMarkerRunner.java
+++ reporting/kraken-docxcod/src/main/java/org/krakenapps/docxcod/FreeMarkerRunner.java
@@ -2,7 +2,7 @@ package org.krakenapps.docxcod;
 
 public class FreeMarkerRunner implements OOXMLProcessor {
 
-	public void run(OOXMLPackage docx) {
+	public void process(OOXMLPackage docx) {
 		// docx 패키지 안의 모든 파일에 대해 FreeMarker 를 돌린다.
 	}
 
