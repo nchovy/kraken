d04f9699358da3835db086d07e516b9be90c12fc
xeraph <devnull@localhost>
2012-09-16 20:16:57 +0900

    solved out of memory issue of confdb exporter with large database file

--- infra/kraken-confdb/src/main/java/org/krakenapps/confdb/file/Exporter.java
+++ infra/kraken-confdb/src/main/java/org/krakenapps/confdb/file/Exporter.java
@@ -3,16 +3,14 @@ package org.krakenapps.confdb.file;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.StringWriter;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
+
 import org.json.JSONException;
 import org.json.JSONWriter;
 import org.krakenapps.codec.Base64;
@@ -39,67 +37,62 @@ public class Exporter {
 		logger.debug("kraken confdb: start export data");
 		db.lock();
 		try {
-			Comparator<String> reverseOrder = new Comparator<String>() {
-				@Override
-				public int compare(String s1, String s2) {
-					return s2.compareTo(s1);
-				}
-			};
+			OutputStreamWriter writer = new OutputStreamWriter(os);
+			JSONWriter jw = new JSONWriter(writer);
 
-			StringWriter writer = new StringWriter(10240);
-			JSONWriter jsonWriter = new JSONWriter(writer);
-			Map<String, Object> m = new TreeMap<String, Object>(reverseOrder);
-			Map<String, Object> metadata = new TreeMap<String, Object>();
-			Map<String, Object> cols = new TreeMap<String, Object>();
+			jw.object();
 
-			metadata.put("version", 1);
-			metadata.put("date", sdf.format(new Date()));
+			jw.key("metadata");
+			jw.object();
+			jw.key("version").value(1);
+			jw.key("date").value(sdf.format(new Date()));
+			jw.endObject();
 
-			m.put("metadata", metadata);
+			jw.key("collections");
+			jw.object();
 
 			for (String name : db.getCollectionNames()) {
-				List<Object> docs = new ArrayList<Object>();
 				ConfigCollection col = db.getCollection(name);
+
+				// collection name
+				jw.key(name);
+
+				// typed doc list
+				jw.array();
+
+				jw.value("list");
+
+				// doc list begin
+				jw.array();
 
 				ConfigIterator it = col.findAll();
 				try {
 					while (it.hasNext()) {
 						Object doc = it.next().getDocument();
-						docs.add(doc);
+						jw.value(insertType(doc));
 					}
 				} finally {
 					it.close();
 				}
-				cols.put(name, insertType(docs));
+
+				// end of doc list
+				jw.endArray();
+
+				// end of typed list
+				jw.endArray();
 			}
 
-			m.put("collections", cols);
+			// end of collection
+			jw.endObject();
 
-			jsonWriter.object();
-
-			for (String key : m.keySet())
-				jsonWriter.key(key).value(m.get(key));
-
-			jsonWriter.endObject();
-
+			// end of master doc
+			jw.endObject();
+			writer.flush();
 			logger.debug("kraken confdb: export complete");
-			writeJsonString(writer.toString(), os);
 		} catch (JSONException e) {
 			throw new IOException(e);
 		} finally {
 			db.unlock();
-		}
-	}
-
-	private void writeJsonString(String json, OutputStream os) throws IOException {
-		OutputStreamWriter writer = null;
-		try {
-			writer = new OutputStreamWriter(os, "utf-8");
-			writer.write(json);
-			writer.flush();
-		} catch (IOException e) {
-			logger.error("kraken confdb: cannot write json", e);
-			throw e;
 		}
 	}
 
