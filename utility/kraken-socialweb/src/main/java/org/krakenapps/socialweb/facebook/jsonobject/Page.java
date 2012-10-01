cbd8d993d9745396b8be87413407dadd5fdf5a6c
tgnice <tgnice@nchovy.com>
2012-10-01 21:59:12 +0900

    rebased top of default 1276 and 1267

--- utility/kraken-socialweb/src/main/java/org/krakenapps/socialweb/facebook/jsonobject/Page.java
+++ utility/kraken-socialweb/src/main/java/org/krakenapps/socialweb/facebook/jsonobject/Page.java
@@ -4,21 +4,21 @@ import org.json.JSONObject;
 
 public class Page implements FacebookGraphObject{
 
-	String id;
-	String name;
-	String link;
-	String category;
-	boolean is_published;
-	boolean can_post;
-	int likes;
-	String location;
-	String phone;
-	int checkins;
-	String pictures;
-	String cover;
-	String website;
-	int talking_about_count;
-	String access_token;
+	private String id;
+	private String name;
+	private String link;
+	private String category;
+	private boolean is_published;
+	private boolean can_post;
+	private int likes;
+	private String location;
+	private String phone;
+	private int checkins;
+	private String pictures;
+	private String cover;
+	private String website;
+	private int talking_about_count;
+	private String access_token;
 	
 	private class FbConnection{
 		
