de6da19e27e5c80d8170403804a8c25e00f5468a
tgnice <tgnice@nchovy.com>
2012-11-13 15:39:30 +0900

    added getter setter and push test

--- utility/kraken-socialweb/src/main/java/org/krakenapps/socialweb/facebook/jsonobject/User.java
+++ utility/kraken-socialweb/src/main/java/org/krakenapps/socialweb/facebook/jsonobject/User.java
@@ -290,6 +290,287 @@ public class User implements FacebookGraphObject{
 		}
 		
 	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getMiddle_name() {
+		return middle_name;
+	}
+
+	public void setMiddle_name(String middle_name) {
+		this.middle_name = middle_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getLocale() {
+		return locale;
+	}
+
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
+
+	public ArrayList<From> getLanguages() {
+		return languages;
+	}
+
+	public void setLanguages(ArrayList<From> languages) {
+		this.languages = languages;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getThird_party_id() {
+		return third_party_id;
+	}
+
+	public void setThird_party_id(String third_party_id) {
+		this.third_party_id = third_party_id;
+	}
+
+	public Installed getInstalled() {
+		return installed;
+	}
+
+	public void setInstalled(Installed installed) {
+		this.installed = installed;
+	}
+
+	public int getTimezone() {
+		return timezone;
+	}
+
+	public void setTimezone(int timezone) {
+		this.timezone = timezone;
+	}
+
+	public String getUpdated_time() {
+		return updated_time;
+	}
+
+	public void setUpdated_time(String updated_time) {
+		this.updated_time = updated_time;
+	}
+
+	public boolean isVerified() {
+		return verified;
+	}
+
+	public void setVerified(boolean verified) {
+		this.verified = verified;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public ArrayList<Cover> getCover() {
+		return cover;
+	}
+
+	public void setCover(ArrayList<Cover> cover) {
+		this.cover = cover;
+	}
+
+	public ArrayList<Devices> getDevices() {
+		return devices;
+	}
+
+	public void setDevices(ArrayList<Devices> devices) {
+		this.devices = devices;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public From getHometown() {
+		return hometown;
+	}
+
+	public void setHometown(From hometown) {
+		this.hometown = hometown;
+	}
+
+	public ArrayList<String> getInterested_in() {
+		return interested_in;
+	}
+
+	public void setInterested_in(ArrayList<String> interested_in) {
+		this.interested_in = interested_in;
+	}
+
+	public From getLocation() {
+		return location;
+	}
+
+	public void setLocation(From location) {
+		this.location = location;
+	}
+
+	public String getPolitical() {
+		return political;
+	}
+
+	public void setPolitical(String political) {
+		this.political = political;
+	}
+
+	public ArrayList<From> getFavorite_athletes() {
+		return favorite_athletes;
+	}
+
+	public void setFavorite_athletes(ArrayList<From> favorite_athletes) {
+		this.favorite_athletes = favorite_athletes;
+	}
+
+	public ArrayList<From> getFavorite_team() {
+		return favorite_team;
+	}
+
+	public void setFavorite_team(ArrayList<From> favorite_team) {
+		this.favorite_team = favorite_team;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+
+	public String getQuotes() {
+		return quotes;
+	}
+
+	public void setQuotes(String quotes) {
+		this.quotes = quotes;
+	}
+
+	public String getRelationship_status() {
+		return relationship_status;
+	}
+
+	public void setRelationship_status(String relationship_status) {
+		this.relationship_status = relationship_status;
+	}
+
+	public String getReligion() {
+		return religion;
+	}
+
+	public void setReligion(String religion) {
+		this.religion = religion;
+	}
+
+	public String getSecurity_settings() {
+		return security_settings;
+	}
+
+	public void setSecurity_settings(String security_settings) {
+		this.security_settings = security_settings;
+	}
+
+	public From getSignificant_other() {
+		return significant_other;
+	}
+
+	public void setSignificant_other(From significant_other) {
+		this.significant_other = significant_other;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public ArrayList<Work> getWork() {
+		return work;
+	}
+
+	public void setWork(ArrayList<Work> work) {
+		this.work = work;
+	}
+
+	public FbConnection getFbConnection() {
+		return fbConnection;
+	}
+
+	public void setFbConnection(FbConnection fbConnection) {
+		this.fbConnection = fbConnection;
+	}
+
 	@Override
 	public int parseJson(JSONObject json) {
 		// TODO Auto-generated method stub
