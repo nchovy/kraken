7a2e31f9e7d7b177bdeb28dad612b0c9f8efaeaf
tgnice <tgnice@nchovy.com>
2012-11-18 20:42:56 +0900

    added Error codes before deprecated error codes

--- utility/kraken-socialweb/src/main/java/org/krakenapps/socialweb/facebook/graphapi/objectcode/ErrorCode.java
+++ utility/kraken-socialweb/src/main/java/org/krakenapps/socialweb/facebook/graphapi/objectcode/ErrorCode.java
@@ -6,10 +6,12 @@ import java.util.Map;
 public enum ErrorCode {
 	/*
 	 * Convention
-	 * 
-	 * "errortype"_"Error Name"
+	 * http://fbdevwiki.com/wiki/Error_codes
+	 * "Errortype"_"Error Name"
 	 * 	
-	 * The Error name of "Front end error" is consisted of upper initial case and lower cases 
+	 * Error types are word sets consisted of initial upper case and rest lower cases without under bar
+	 * 
+	 * The Error name of "Front end error" is consisted of initial upper case and rest lower cases 
 	 * The Error name of "Back end error" is consisted of upper case only and bonded with under bar  
 	 *   
 	 */
@@ -84,6 +86,142 @@ public enum ErrorCode {
 	UserPermission_API_EC_PERMISSION_READ_MAILBOX(298),	 //Reading mailbox messages requires the extended permission read_mailbox	 message.getThreadsInFolder
 	UserPermission_API_EC_PERMISSION_RSVP_EVENT(299),	// RSVPing to events requires the extended permission create_rsvp
 	
+	DataEditing_API_EC_EDIT(300),	// Edit failure	
+	DataEditing_API_EC_EDIT_USER_DATA(310),	 //User data edit failure	
+	DataEditing_API_EC_EDIT_PHOTO(320),	// Photo edit failure	
+	DataEditing_API_EC_EDIT_ALBUM_SIZE(321),	// Album is full	
+	DataEditing_API_EC_EDIT_PHOTO_TAG_SUBJECT(322),	 //Invalid photo tag subject	
+	DataEditing_API_EC_EDIT_PHOTO_TAG_PHOTO(323),	 //Cannot tag photo already visible on Facebook	
+	DataEditing_API_EC_EDIT_PHOTO_FILE(324),	// Missing or invalid image file	
+	DataEditing_API_EC_EDIT_PHOTO_PENDING_LIMIT(325),	 //Too many unapproved photos pending	
+	DataEditing_API_EC_EDIT_PHOTO_TAG_LIMIT(326),	 //Too many photo tags pending	
+	DataEditing_API_EC_EDIT_ALBUM_REORDER_PHOTO_NOT_IN_ALBUM(327),	 //Input array contains a photo not in the album	
+	DataEditing_API_EC_EDIT_ALBUM_REORDER_TOO_FEW_PHOTOS(328),	// Input array has too few photos	
+	DataEditing_API_EC_MALFORMED_MARKUP(329),	// Template data must be a JSON-encoded dictionary, of the form {'key-1': 'value-1', 'key-2': 'value-2', ...}	
+	DataEditing_API_EC_EDIT_MARKUP(330),	// Failed to set markup	
+	DataEditing_API_EC_EDIT_FEED_TOO_MANY_USER_CALLS(330),	 //Feed publication request limit reached	
+	DataEditing_API_EC_EDIT_FEED_TOO_MANY_USER_ACTION_CALLS(341),	 //Feed action request limit reached	
+	DataEditing_API_EC_EDIT_FEED_TITLE_LINK(342),	// Feed story title can have at most one href anchor	
+	DataEditing_API_EC_EDIT_FEED_TITLE_LENGTH(343),	// Feed story title is too long	
+	DataEditing_API_EC_EDIT_FEED_TITLE_NAME(344),	// Feed story title can have at most one fb:userlink and must be of the user whose action is being reported	
+	DataEditing_API_EC_EDIT_FEED_TITLE_BLANK(345),	// Feed story title rendered as blank	
+	DataEditing_API_EC_EDIT_FEED_BODY_LENGTH(346),	// Feed story body is too long	
+	DataEditing_API_EC_EDIT_FEED_PHOTO_SRC(347),	// Feed story photo could not be accessed or proxied	
+	DataEditing_API_EC_EDIT_FEED_PHOTO_LINK(348),	// Feed story photo link invalid	
+	DataEditing_API_EC_EDIT_VIDEO_SIZE(350),	// Video file is too large	 video.upload
+	DataEditing_API_EC_EDIT_VIDEO_INVALID_FILE(351),	 //Video file was corrupt or invalid	 
+	DataEditing_API_EC_EDIT_VIDEO_INVALID_TYPE(352),	// Video file format is not supported	 
+	DataEditing_API_EC_EDIT_VIDEO_FILE(353),	//Missing video file	 video.upload
+	DataEditing_API_EC_EDIT_VIDEO_NOT_TAGGED(354),	// User is not tagged in this video	
+	DataEditing_API_EC_EDIT_VIDEO_ALREADY_TAGGED(355),	// User is already tagged in this video	
+	DataEditing_API_EC_EDIT_FEED_TITLE_ARRAY(360),	// Feed story title_data argument was not a valid JSON-encoded array	
+	DataEditing_API_EC_EDIT_FEED_TITLE_PARAMS(361),	// Feed story title template either missing required parameters, or did not have all parameters defined in title_data array	
+	DataEditing_API_EC_EDIT_FEED_BODY_ARRAY(362),	// Feed story body_data argument was not a valid JSON-encoded array	
+	DataEditing_API_EC_EDIT_FEED_BODY_PARAMS(363),	// Feed story body template either missing required parameters, or did not have all parameters defined in body_data array	
+	DataEditing_API_EC_EDIT_FEED_PHOTO(364),	 //Feed story photos could not be retrieved, or bad image links were provided	
+	DataEditing_API_EC_EDIT_FEED_TEMPLATE(365),	 //The template for this story does not match any templates registered for this application	
+	DataEditing_API_EC_EDIT_FEED_TARGET(366),	 //One or more of the target ids for this story are invalid. They must all be ids of friends of the acting user	
+	DataEditing_API_EC_EDIT_FEED_MARKUP(367),	 //The template data provided doesn't cover the entire token set needed to publish the story	
+	DataEditing_Warning(368),// This Message Contains Blocked Content: Some content in this message has been reported as abusive by Facebook users.	
+	DataEditing_API_EC_USERS_CREATE_INVALID_EMAIL(370),	 //The email address you provided is not a valid email address	
+	DataEditing_API_EC_USERS_CREATE_EXISTING_EMAIL(371),	 //The email address you provided belongs to an existing account	
+	DataEditing_API_EC_USERS_CREATE_BIRTHDAY(372),	 //The birthday provided is not valid	
+	DataEditing_API_EC_USERS_CREATE_PASSWORD(373),	 //The password provided is too short or weak	
+	DataEditing_API_EC_USERS_REGISTER_INVALID_CREDENTIAL(374),	 //The login credential you provided is invalid.	
+	DataEditing_API_EC_USERS_REGISTER_CONF_FAILURE(375),	 //Failed to send confirmation message to the specified login credential.	
+	DataEditing_API_EC_USERS_REGISTER_EXISTING(376),	 //The login credential you provided belongs to an existing account	
+	DataEditing_API_EC_USERS_REGISTER_DEFAULT_ERROR(377),	 //Sorry, we were unable to process your registration.	
+	DataEditing_API_EC_USERS_REGISTER_PASSWORD_BLANK(378),	// Your password cannot be blank. Please try another.	
+	DataEditing_API_EC_USERS_REGISTER_PASSWORD_INVALID_CHARS(379),	// Your password contains invalid characters. Please try another.	
+	DataEditing_API_EC_USERS_REGISTER_PASSWORD_SHORT(380),	// Your password must be at least 6 characters long. Please try another.	
+	DataEditing_API_EC_USERS_REGISTER_PASSWORD_WEAK(381),	// Your password should be more secure. Please try another.	
+	DataEditing_API_EC_USERS_REGISTER_USERNAME_ERROR(382),	// Our automated system will not approve this name.	
+	DataEditing_API_EC_USERS_REGISTER_MISSING_INPUT(383),	// You must fill in all of the fields.	
+	DataEditing_API_EC_USERS_REGISTER_INCOMPLETE_BDAY(384),	 //You must indicate your full birthday to register.	
+	DataEditing_API_EC_USERS_REGISTER_INVALID_EMAIL(385),	// Please enter a valid email address.	
+	DataEditing_API_EC_USERS_REGISTER_EMAIL_DISABLED(386),	// The email address you entered has been disabled. Please contact disabled@facebook.com with any questions.	
+	DataEditing_API_EC_USERS_REGISTER_ADD_USER_FAILED(387),	// There was an error with your registration. Please try registering again.	
+	DataEditing_API_EC_USERS_REGISTER_NO_GENDER(388),	 //Please select either Male or Female.
+	
+	Authentication_API_EC_AUTH_EMAIL(400),	 //Invalid email address	
+	Authentication_API_EC_AUTH_LOGIN(401),	// Invalid username or password	
+	Authentication_API_EC_AUTH_SIG(402),	// Invalid application auth sig	
+	Authentication_API_EC_AUTH_TIME(403),	// Invalid timestamp for authentication	
+	
+	Session_API_EC_SESSION_TIMED_OUT(450),	 //Session key specified has passed its expiration time	
+	Session_API_EC_SESSION_METHOD(451),	 //Session key specified cannot be used to call this method	
+	Session_API_EC_SESSION_INVALID(452),	 //Session key invalid. This could be because the session key has an incorrect format, or because the user has revoked this session	
+	Session_API_EC_SESSION_REQUIRED(453),	// A session key is required for calling this method	
+	Session_API_EC_SESSION_REQUIRED_FOR_SECRET(454),	// A session key must be specified when request is signed with a session secret	
+	Session_API_EC_SESSION_CANNOT_USE_SESSION_SECRET(455),	// A session secret is not permitted to be used with this type of session key
+	
+	ApplicationMessaging_API_EC_MESG_BANNED(500),//	 Message contains banned content	
+	ApplicationMessaging_API_EC_MESG_NO_BODY(501),//	 Missing message body	
+	ApplicationMessaging_API_EC_MESG_TOO_LONG(502),	 //Message is too long	
+	ApplicationMessaging_API_EC_MESG_RATE(503),//	 User has sent too many messages	
+	ApplicationMessaging_API_EC_MESG_INVALID_THREAD(504),//	 Invalid reply thread id	
+	ApplicationMessaging_API_EC_MESG_INVALID_RECIP(505),//	 Invalid message recipient	
+	ApplicationMessaging_API_EC_POKE_INVALID_RECIP(510),	// Invalid poke recipient	
+	ApplicationMessaging_API_EC_POKE_OUTSTANDING(511),//	 There is a poke already outstanding	
+	ApplicationMessaging_API_EC_POKE_RATE(512),//	 User is poking too fast	
+	ApplicationMessaging_API_EC_POKE_USER_BLOCKED(513),//	 User cannot poke via API
+	
+	FQL_EC_UNKNOWN_ERROR(600),//	 An unknown error occurred in FQL	 fql.query,fql.multiquery
+	FQL_EC_PARSER_ERROR(601),//	 Error while parsing FQL statement	 fql.query,fql.multiquery
+	FQL_EC_UNKNOWN_FIELD(602),//	 The field you requested does not exist	 fql.query,fql.multiquery
+	FQL_EC_UNKNOWN_TABLE(603),//	 The table you requested does not exist	 fql.query,fql.multiquery
+	FQL_EC_NO_INDEX(604),//	 Your statement is not indexable	 fql.query,fql.multiquery
+	FQL_EC_UNKNOWN_FUNCTION(605),//	 The function you called does not exist	 fql.query,fql.multiquery
+	FQL_EC_INVALID_PARAM(606),//	 Wrong number of arguments passed into the function	 fql.query,fql.multiquery
+	FQL_EC_INVALID_FIELD(607),//	 FQL field specified is invalid in this context.	 fql.query*,fql.multiquery
+	FQL_EC_INVALID_SESSION(608),//	 An invalid session was specified	 fql.query,fql.multiquery
+	FQL_EC_UNSUPPORTED_APP_TYPE(609),//	 FQL field specified is invalid in this context.	 fql.query*,fql.multiquery
+	FQL_EC_SESSION_SECRET_NOT_ALLOWED(610),//	 FQL field specified is invalid in this context.	 fql.query*,fql.multiquery
+	FQL_EC_DEPRECATED_TABLE(611),//	 FQL field specified is invalid in this context.	 fql.query*,fql.multiquery
+	FQL_EC_EXTENDED_PERMISSION(612),//	 The stream requires an extended permission	 fql.query,fql.multiquery
+	FQL_EC_RATE_LIMIT_EXCEEDED(613),//	 Calls to stream have exceeded the rate of 100 calls per 600 seconds.	 fql.query,fql.multiquery
+	FQL_EC_UNRESOLVED_DEPENDENCY(614),//	 Unresolved dependency in multiquery	 fql.multiquery
+	FQL_EC_INVALID_SEARCH(615),//	 This search is invalid	 fql.query,fql.multiquery
+	FQL_EC_TOO_MANY_FRIENDS_FOR_PRELOAD(617),//	 The user you queried against has too many friends to be used with Preload FQL, in order to avoid out of memory errors	 fql.query,fql.multiquery
+	
+	Ref_API_EC_REF_SET_FAILED(700),//	 Unknown failure in storing ref data. Please try again.
+	
+	ApplicationIntegration_API_EC_FB_APP_UNKNOWN_ERROR(750),//	 Unknown Facebook application integration failure.	
+	ApplicationIntegration_API_EC_FB_APP_FETCH_FAILED(751),//	 Fetch from remote site failed.	
+	ApplicationIntegration_API_EC_FB_APP_NO_DATA(752),//	 Application returned no data. This may be expected or represent a connectivity error.	
+	ApplicationIntegration_API_EC_FB_APP_NO_PERMISSIONS(753),//	 Application returned user had invalid permissions to complete the operation.	
+	ApplicationIntegration_API_EC_FB_APP_TAG_MISSING(754),//	 Application returned data, but no matching tag found. This may be expected.	
+	ApplicationIntegration_API_EC_FB_APP_DB_FAILURE(755),//	 The database for this object failed.
+	
+	ApplicationInformation_API_EC_NO_SUCH_APP(900),//	 No such application exists.
+	
+	BatchAPI_API_BATCH_TOO_MANY_ITEMS(950),//	 Each batch API can not contain more than 20 items	
+	BatchAPI_API_EC_BATCH_ALREADY_STARTED(951),//	 begin_batch already called, please make sure to call end_batch first.	
+	BatchAPI_API_EC_BATCH_NOT_STARTED(952),//	 end_batch called before begin_batch.	
+	BatchAPI_API_EC_BATCH_METHOD_NOT_ALLOWED_IN_BATCH_MODE(953),//	 This method is not allowed in batch mode.
+	
+	EventAPI_API_EC_EVENT_INVALID_TIME(1000),//	 Invalid time for an event.	 events.edit
+	EventAPI_API_EC_EVENT_NAME_LOCKED(1001),//	 You are no longer able to change the name of this event.
+	
+	LiveMessage_API_EC_LIVEMESSAGE_SEND_FAILED(1100),//	 An error occurred while sending the LiveMessage.	liveMessage.send
+	LiveMessage_API_EC_LIVEMESSAGE_EVENT_NAME_TOO_LONG(1101),//	 The event_name parameter must be no longer than 128 bytes.	liveMessage.send
+	LiveMessage_API_EC_LIVEMESSAGE_MESSAGE_TOO_LONG(1102),//	 The message parameter must be no longer than 1024 bytes
+	
+	Chat_API_EC_CHAT_SEND_FAILED(1200),//	 An error occurred while sending the message
+	
+	FacebookPage_API_EC_PAGES_CREATE(1201),//	 You have created too many pages	
+
+	FacebookLinks_API_EC_SHARE_BAD_URL(1500),//	 The url you supplied is invalid	
+	
+	FacebookNotes_API_EC_NOTE_CANNOT_MODIFY(1600),//	 The user does not have permission to modify this note.
+	
+	Comment_API_EC_COMMENTS_UNKNOWN(1700),//	 An unknown error has occurred.	
+	Comment_API_EC_COMMENTS_POST_TOO_LONG(1701),//	 The specified post was too long.	
+	Comment_API_EC_COMMENTS_DB_DOWN(1702),//	 The comments database is down.	
+	Comment_API_EC_COMMENTS_INVALID_XID(1703),//	 The specified xid is not valid. xids can only contain letters, numbers, and underscores	
+	Comment_API_EC_COMMENTS_INVALID_UID(1704),//	 The specified user is not a user of this application	
+	Comment_API_EC_COMMENTS_INVALID_POST(1705),//	 There was an error during posting.	
+	Comment_API_EC_COMMENTS_INVALID_REMOVE(1706),//	 While attempting to remove the post.
 	
 	CreditsFrontEnd_UnknownFacebookSystemIssue(1383001),
 	CreditsFrontEnd_InvalidParameters(1383002),	//	Developer called with the incorrect parameters.
