359d2cb9e7eaf6fab15476cb5bdc9876d831cd4f
gotoweb <igotoweb@gmail.com>
2013-01-07 16:45:27 +0900

    added javascript resources for webconsole

---
+++ infra/kraken-webconsole/src/main/resources/WEB-INF/package/system/orgchart/testdata2.js
@@ -0,0 +1,149 @@
+define([], function() {
+
+return {
+    "areas": [
+        {
+            "guid": "abb0de7a-eccb-4b51-a2e1-68d193e5e391",
+            "created": "2012-06-20 14:31:07+0900",
+            "description": "새우",
+            "name": "ㅇㅇㅇ/",
+            "value": 40
+        },
+        {
+            "guid": "6eede39b-7ccf-4ef7-b9c9-d68025064526",
+            "created": "2012-07-13 17:02:53+0900",
+            "description": "고래",
+            "name": "안녕",
+            "value": 31
+        },
+        {
+            "guid": "932d2a08-4aad-4ef8-8151-48918a08f39e",
+            "created": "2012-07-20 11:38:21+0900",
+            "description": "멸치",
+            "name": "엔초비",
+            "value": 52
+        },
+        {
+            "guid": "feb3ceb5-18f5-4115-a725-7f75f8c8942c",
+            "created": "2012-07-20 11:38:23+0900",
+            "description": "",
+            "name": "EMS",
+            "value": 58
+        },
+        {
+            "guid": "ddd1b42d-9f9a-4e4f-a426-f201da05f4bf",
+            "created": "2012-07-20 13:50:09+0900",
+            "description": "",
+            "name": "PS",
+            "value": 16
+        },
+        {
+            "guid": "abb0de7a-eccb-4b51-a2e1-68d193e5e391",
+            "created": "2012-06-20 14:31:07+0900",
+            "description": null,
+            "name": "NS3",
+            "value": 40
+        },
+        {
+            "guid": "6eede39b-7ccf-4ef7-b9c9-d68025064526",
+            "created": "2012-07-13 17:02:53+0900",
+            "description": "멸치",
+            "name": "NCHOVY",
+            "value": 31
+        },
+        {
+            "guid": "932d2a08-4aad-4ef8-8151-48918a08f39e",
+            "created": "2012-07-20 11:38:21+0900",
+            "description": "",
+            "name": "UX",
+            "value": 52
+        },
+        {
+            "guid": "feb3ceb5-18f5-4115-a725-7f75f8c8942c",
+            "created": "2012-07-20 11:38:23+0900",
+            "description": "",
+            "name": "경영기획실",
+            "value": 58
+        },
+        {
+            "guid": "ddd1b42d-9f9a-4e4f-a426-f201da05f4bf",
+            "created": "2012-07-20 13:50:09+0900",
+            "description": "",
+            "name": "CERT",
+            "value": 16
+        },
+
+        {
+            "guid": "abb0de7a-eccb-4b51-a2e1-68d193e5e391",
+            "created": "2012-06-20 14:31:07+0900",
+            "description": null,
+            "name": "/a",
+            "value": 40
+        },
+        {
+            "guid": "6eede39b-7ccf-4ef7-b9c9-d68025064526",
+            "created": "2012-07-13 17:02:53+0900",
+            "description": "멸치",
+            "name": "엔초비a",
+            "value": 31
+        },
+        {
+            "guid": "932d2a08-4aad-4ef8-8151-48918a08f39e",
+            "created": "2012-07-20 11:38:21+0900",
+            "description": "",
+            "name": "NS2a",
+            "value": 52
+        },
+        {
+            "guid": "feb3ceb5-18f5-4115-a725-7f75f8c8942c",
+            "created": "2012-07-20 11:38:23+0900",
+            "description": "",
+            "name": "EMSa",
+            "value": 58
+        },
+        {
+            "guid": "ddd1b42d-9f9a-4e4f-a426-f201da05f4bf",
+            "created": "2012-07-20 13:50:09+0900",
+            "description": "",
+            "name": "PSa",
+            "value": 16
+        },
+        {
+            "guid": "abb0de7a-eccb-4b51-a2e1-68d193e5e391",
+            "created": "2012-06-20 14:31:07+0900",
+            "description": null,
+            "name": "NS3a",
+            "value": 40
+        },
+        {
+            "guid": "6eede39b-7ccf-4ef7-b9c9-d68025064526",
+            "created": "2012-07-13 17:02:53+0900",
+            "description": "멸치",
+            "name": "NCHOVYa",
+            "value": 31
+        },
+        {
+            "guid": "932d2a08-4aad-4ef8-8151-48918a08f39e",
+            "created": "2012-07-20 11:38:21+0900",
+            "description": "",
+            "name": "UXa",
+            "value": 52
+        },
+        {
+            "guid": "feb3ceb5-18f5-4115-a725-7f75f8c8942c",
+            "created": "2012-07-20 11:38:23+0900",
+            "description": "",
+            "name": "경영기획실a",
+            "value": 58
+        },
+        {
+            "guid": "ddd1b42d-9f9a-4e4f-a426-f201da05f4bf",
+            "created": "2012-07-20 13:50:09+0900",
+            "description": "",
+            "name": "CERTa",
+            "value": 16
+        }
+    ]
+}
+
+});
